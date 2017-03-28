6befe92be430f03f9c49c82eaa85631b145aabc5
Massimo Perini <perinimassimo1995@gmail.com>
2017-03-28 20:42:01 +0200

    First Commit

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String txt = JOptionPane.showInputDialog("Inserire testo");
+        String txt = JOptionPane.showInputDialog("Inserire testo"); 
         System.out.println(txt);
     }
 }
