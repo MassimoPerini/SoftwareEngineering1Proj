46568baccb3358fa64cb0efe9b154f80af682950
GabrielePrato <gabrieleprato.work@gmail.com>
2017-05-20 19:17:55 +0200

    Costruzione Board, controllatela e guardate soprattuto i todo, nel caso vedete che siano rispettate tutte le regole nella maniera più corretta possibile

--- src/main/java/it/polimi/ingsw/GC_06/Board/TowerFloor.java
+++ src/main/java/it/polimi/ingsw/GC_06/Board/TowerFloor.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw.GC_06.Board;
 
 import it.polimi.ingsw.GC_06.Card.DevelopmentCard;
+import it.polimi.ingsw.GC_06.playerTools.Effect;
+
+import java.util.ArrayList;
+
 import it.polimi.ingsw.GC_06.FamilyMember;
 
 /**
@@ -11,13 +15,10 @@ public class TowerFloor {
     private DevelopmentCard card;
     private ActionPlace actionPlace;
 
-    public TowerFloor(int requiredValue)
+    public TowerFloor(ArrayList<Effect> effect, int costo, DevelopmentCard card )
     {
-        this.actionPlace = new SingleActionPlace(requiredValue);
+        this.actionPlace = new ActionPlaceTower(effect , costo);
+        this.card = card;
     }
 
-    public DevelopmentCard addFamilyMember(FamilyMember member) {
-        actionPlace.addFamilyMember(member);
-        return card;
-    }
 }
