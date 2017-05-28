bf99dad8e1d40ab4a9e1700aff28fb56c0eb0731
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-05-28 09:41:42 +0200

    Dovrebbe andare!! a meno di qualche piccola cosa

---
+++ src/main/java/it/polimi/ingsw/GC_06/model/Board/Prova.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.GC_06.model.Board;
+
+/**
+ * Created by giuseppe on 5/28/17.
+ */
+public class Prova {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
