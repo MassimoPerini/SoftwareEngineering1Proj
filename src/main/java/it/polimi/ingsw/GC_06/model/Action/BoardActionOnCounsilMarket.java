c5b44faddba6764ee7d41db23548b16033bea022
GabrielePrato <gabrieleprato.work@gmail.com>
2017-05-30 17:42:48 +0200

    Fix on name class

--- src/main/java/it/polimi/ingsw/GC_06/model/Action/BoardActionOnCounsilMarket.java
+++ src/main/java/it/polimi/ingsw/GC_06/model/Action/BoardActionOnCounsilMarket.java
@@ -1,10 +1,9 @@
 package it.polimi.ingsw.GC_06.model.Action;
 
-import it.polimi.ingsw.GC_06.model.Board.Market;
+import it.polimi.ingsw.GC_06.model.Board.Council;
 import it.polimi.ingsw.GC_06.model.Effect.Effect;
 import it.polimi.ingsw.GC_06.FamilyMember;
 import it.polimi.ingsw.GC_06.model.playerTools.Player;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +11,19 @@ import java.util.List;
 /**
  * Created by giuseppe on 5/20/17.
  */
-public class MarketBoardAction extends BoardAction {
+public class BoardActionOnCounsilMarket extends BoardAction {
 
     private Player player;
-    private Market component;
-    int index;
+    private Council component;
+    private int index;
 
+    public BoardActionOnCounsilMarket(Player player, Council component, FamilyMember familyMember, int index, int value) {
 
-    public MarketBoardAction(Player player, FamilyMember familyMember, int index, Market component, int value) {
         super(familyMember, value);
-    	this.player = player;
-        this.index = index;
         this.component = component;
+        this.player = player;
+        this.index = index;
+        
     }
 
     @Override
@@ -31,19 +31,15 @@ public class MarketBoardAction extends BoardAction {
 
         component.addFamilyMember(getFamilyMember(), index);
         List<Effect> effects = component.getEffect(index);
-        //facciamo un ciclo
         for(Effect effect : effects){
             effect.execute(player);
         }
+
     }
-/**
-    public void setPlayer(Player player) {
-        this.player = player;
-    }*/
 
     @Override
     public boolean isAllowed() {
-        return component.isAllowed(getFamilyMember(), index) ;
+        return component.isAllowed(getFamilyMember(), index);
     }
-
 }
+
