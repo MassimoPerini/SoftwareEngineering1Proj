fbf7353d582135eb6d6a36ab43152e1672fcdf26
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-06-04 18:39:07 +0200

    PiccolaModifica che avevo dimenticato

--- src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/BonusMalusSet.java
+++ src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/BonusMalusSet.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.GC_06.model.BonusMalus;
 
+import it.polimi.ingsw.GC_06.model.Action.PlayType;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,19 +11,19 @@ import java.util.HashMap;
 public class BonusMalusSet {
 
     /** secondome va un po' ripensato*/
-    HashMap<String,ArrayList<BonusMalusOnResources>> bonusMalusOnResources;
-    HashMap<String,ArrayList<BonusMalusOnAction>> bonusMalusOnAction;
+    HashMap<PlayType,ArrayList<BonusMalusOnResources>> bonusMalusOnResources;
+    HashMap<PlayType,ArrayList<BonusMalusOnAction>> bonusMalusOnAction;
 
     public BonusMalusSet() {
-        this.bonusMalusOnResources = new HashMap<String,ArrayList<BonusMalusOnResources>>();
-        this.bonusMalusOnAction = new HashMap<String,ArrayList<BonusMalusOnAction>>();
+        this.bonusMalusOnResources = new HashMap<PlayType,ArrayList<BonusMalusOnResources>>();
+        this.bonusMalusOnAction = new HashMap<PlayType,ArrayList<BonusMalusOnAction>>();
     }
 
-    public HashMap<String, ArrayList<BonusMalusOnResources>> getBonusMalusOnResources() {
+    public HashMap<PlayType, ArrayList<BonusMalusOnResources>> getBonusMalusOnResources() {
         return bonusMalusOnResources;
     }
 
-    public HashMap<String, ArrayList<BonusMalusOnAction>> getBonusMalusOnAction() {
+    public HashMap<PlayType, ArrayList<BonusMalusOnAction>> getBonusMalusOnAction() {
         return bonusMalusOnAction;
     }
 }
