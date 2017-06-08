75f2920f8190412433227f0f9f74f6baa129751d
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-06-08 11:31:55 +0200

    TestBonusMalusOnActions

--- src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/BonusMalusOnAction.java
+++ src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/BonusMalusOnAction.java
@@ -11,21 +11,21 @@ public class BonusMalusOnAction {
 
     /** serve se viene fatta una azione torre*/
     private String colourTarget;
-    private LinkedList<String> familyMemberColours;
-    private int malusEntity;
+    private LinkedList<String> familyMemberColours; /** i colori dei familiari su cui questo bonus impatta */
+    private int bonusMalusEntity;
     /** actionType serve ad identificare l'azione sulla quale il bonus o il malus agirà*/
     private ActionType actionType;
 
     public BonusMalusOnAction(String colourTarget, int malusEntity,ActionType actionType,LinkedList<String> familyMemberColours) {
         this.colourTarget = colourTarget;
-        this.malusEntity = malusEntity;
+        this.bonusMalusEntity = malusEntity;
         this.actionType = actionType;
         this.familyMemberColours = familyMemberColours;
     }
 
         /** con questo metodo riduciamo il valore dell'azione, cambiando il punteggio del familiare*/
     public void modify(FamilyMember familyMember) {
-        int newValue = familyMember.getValue() - malusEntity;
+        int newValue = familyMember.getValue() + bonusMalusEntity;
         familyMember.setValue(newValue);
     }
 
