e545e6693e3cf4d89155e3ca28c308de7a87e8cc
Massimo Perini <perinimassimo1995@gmail.com>
2017-06-09 11:36:31 +0200

    Packages

---
+++ src/main/java/it/polimi/ingsw/GC_06/Network/Client/Test1.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.GC_06.Network.Client;
+
+/**
+ * Created by massimo on 09/06/17.
+ */
+public class Test1 {
+}
---
+++ src/main/java/it/polimi/ingsw/GC_06/Network/Server/Test2.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.GC_06.Network.Server;
+
+/**
+ * Created by massimo on 09/06/17.
+ */
+public class Test2 {
+}
