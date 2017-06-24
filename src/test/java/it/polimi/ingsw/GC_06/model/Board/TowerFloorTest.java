51854294123371a51f4228272f9c985d698415bb
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-06-24 16:19:38 +0200

    TowerFloor test

--- src/test/java/it/polimi/ingsw/GC_06/model/Board/TowerFloorTest.java
+++ src/test/java/it/polimi/ingsw/GC_06/model/Board/TowerFloorTest.java
@@ -3,72 +3,73 @@ package it.polimi.ingsw.GC_06.model.Board;
 import it.polimi.ingsw.GC_06.model.Card.DevelopmentCard;
 import it.polimi.ingsw.GC_06.model.Card.Requirement;
 import it.polimi.ingsw.GC_06.model.Effect.Effect;
+import it.polimi.ingsw.GC_06.model.Effect.ProdHarvEffect;
 import it.polimi.ingsw.GC_06.model.playerTools.FamilyMember;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import static junit.framework.TestCase.assertTrue;
 
 /**
- * Created by giuseppe on 6/23/17.
+ * Created by giuseppe on 6/24/17.
  */
 public class TowerFloorTest {
 
-    private DevelopmentCard developmentCard;
     private ActionPlace actionPlace;
-    private TowerFloor  towerFloor;
+    private ActionPlace actionPlaceFixed;
+    private DevelopmentCard developmentCard;
+    private TowerFloor towerFloor;
+    private FamilyMember familyMember;
+    private TowerFloor fixedTowerFloor;
 
     @Before
     public void setUp(){
 
-        developmentCard = new DevelopmentCard("Peppe",1,new LinkedList<Requirement>(),new LinkedList<Effect>(),"YELLOW",new HashMap<>());
-        actionPlace = new ActionPlaceFixed(new LinkedList<Effect>(),2,1);
-
+        actionPlace = new ActionPlace(new LinkedList<Effect>(),4);
+        developmentCard = new DevelopmentCard("peppe",1,new LinkedList<Requirement>(), new LinkedList<Effect>(), "yellow",new HashMap<Integer,List<ProdHarvEffect>>());
         towerFloor = new TowerFloor(actionPlace,developmentCard);
+        familyMember = new FamilyMember("green","peppe");
+        actionPlaceFixed = new ActionPlaceFixed(new LinkedList<Effect>(),2,1);
+        fixedTowerFloor = new TowerFloor(actionPlaceFixed,developmentCard);
     }
 
-
-/*
-    @Test(expected = IllegalStateException.class)
-    public void putFamilyMemberTest(){
-
-        FamilyMember familyMember = new FamilyMember("YELLOW","Peppe");
-        towerFloor.addFamilyMember(familyMember);
-        assertTrue(actionPlace.getMembers().size() == 1);
-
-    }
-*/
     @Test
-    public void putFamilyMemberTest2(){
-        FamilyMember familyMember = new FamilyMember("YELLOW","Peppe");
-        familyMember.setValue(10);
+    public void addFamilyMemberTest(){
         towerFloor.addFamilyMember(familyMember);
-        assertTrue(actionPlace.getMembers().size() == 1);
-    }
-/*
-    @Test(expected = IllegalStateException.class)
-    public void denyAccessTest(){
-        FamilyMember familyMember = new FamilyMember("YELLOW","Peppe");
-        familyMember.setValue(10);
-        FamilyMember familyMember1 = new FamilyMember("YELLOW","Peppe");
-        familyMember1.setValue(10);
-        towerFloor.addFamilyMember(familyMember1);
-        towerFloor.addFamilyMember(familyMember);
+        assertTrue(towerFloor.getActionPlace().getMembers().size() == 1);
+        fixedTowerFloor.addFamilyMember(familyMember);
+        assertTrue(towerFloor.getActionPlace().getMembers().size() == 1);
 
-        assertTrue(2 == actionPlace.getMembers().size() );
+
     }
-*/
+    /**
+    @Test(expected = IllegalStateException.class)
+    public void notAddFamilyMember(){
+        fixedTowerFloor.addFamilyMember(familyMember);
+        fixedTowerFloor.addFamilyMember(familyMember);
+        assertTrue(towerFloor.getActionPlace().getMembers().size() == 2);
+    }*/
+
     @Test
-    public void removeFamilyMemberTest(){
-        FamilyMember familyMember = new FamilyMember("YELLOW","Peppe");
-        familyMember.setValue(10);
+
+    public void removeFamilyMemebr(){
+
         towerFloor.addFamilyMember(familyMember);
         towerFloor.removeFamilyMembers();
-        assertTrue(0 == actionPlace.getMembers().size() );
+
+        assertTrue(0 == actionPlace.getMembers().size());
     }
 
+    @Test
 
+    public void pickCard(){
+
+        DevelopmentCard developmentCard = towerFloor.pickCard();
+        assertTrue(developmentCard.getIdColour().equals("yellow"));
+    }
 }
