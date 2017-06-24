f72695a21acb3a4f378de90a95e0fb8cf8468614
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-06-24 15:48:24 +0200

    TowerFloor test

--- src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/SpecialBonusMalus.java
+++ src/main/java/it/polimi/ingsw/GC_06/model/BonusMalus/SpecialBonusMalus.java
@@ -3,5 +3,37 @@ package it.polimi.ingsw.GC_06.model.BonusMalus;
 /**
  * Created by giuseppe on 6/12/17.
  */
+
+import it.polimi.ingsw.GC_06.model.Board.ActionPlace;
+import it.polimi.ingsw.GC_06.model.Effect.Effect;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/** bonus e malus che cancella gli effetti da uno spazio azione*/
 public class SpecialBonusMalus {
+
+    ActionType actionType;
+
+    public SpecialBonusMalus(ActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    public void modify(ActionPlace actionPlace){
+
+
+        for (Effect actionPlaceEffect : actionPlace.getEffects()) {
+            actionPlaceEffect = null;
+        }
+    }
+
+    public boolean isAllowed(ActionType actionType){
+        if(this.actionType.equals(actionType)){
+            return true;
+        }
+        else
+            return false;
+    }
+
 }
--- src/test/java/it/polimi/ingsw/GC_06/model/Board/TowerTest.java
+++ src/test/java/it/polimi/ingsw/GC_06/model/Board/TowerTest.java
@@ -1,9 +1,36 @@
 package it.polimi.ingsw.GC_06.model.Board;
 
+import it.polimi.ingsw.GC_06.model.Card.DevelopmentCard;
+import it.polimi.ingsw.GC_06.model.Card.Requirement;
+import it.polimi.ingsw.GC_06.model.Effect.Effect;
+import org.junit.Before;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by giuseppe on 6/23/17.
  */
 public class TowerTest {
 
+    private ActionPlace actionPlace;
+    private TowerFloor towerFloor;
+    private TowerFloor towerFloor1;
+    private TowerFloor towerFloor2;
+    private TowerFloor towerFloor3;
+    private Tower tower;
+    private DevelopmentCard developmentCard;
+    private List<TowerFloor> towerFloors = new LinkedList<>();
+
+    @Before
+    public void setUp(){
+        developmentCard = new DevelopmentCard("Peppe",1,new LinkedList<Requirement>(),new LinkedList<Effect>(),"YELLOW",new HashMap<>());
+        actionPlace = new ActionPlace(new LinkedList<Effect>(),2);
+        towerFloor = new TowerFloor(actionPlace,developmentCard);
+        towerFloor1 = new TowerFloor(actionPlace,developmentCard);
+        towerFloor2 = new TowerFloor(actionPlace,developmentCard);
+        towerFloor3  = new TowerFloor(actionPlace,developmentCard);
+    }
 
 }
