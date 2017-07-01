811daeca12b0f3ff5e0f0da49a1abfe48cbd90a5
russogiuseppe <giuseppe.russo1212@gmail.com>
2017-07-01 11:40:21 +0200

    effectOnCondition

---
+++ src/test/java/it/polimi/ingsw/GC_06/model/Effect/EffectOnConditionsTest.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.GC_06.model.Effect;
+
+import it.polimi.ingsw.GC_06.model.Action.PickCard.DefaulEventManagerFake;
+import it.polimi.ingsw.GC_06.model.BonusMalus.ActionType;
+import it.polimi.ingsw.GC_06.model.Resource.Resource;
+import it.polimi.ingsw.GC_06.model.State.Game;
+import it.polimi.ingsw.GC_06.model.playerTools.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by giuseppe on 7/1/17.
+ */
+public class EffectOnConditionsTest {
+
+    private Game game;
+    private Player player;
+    private EffectOnConditions effectOnConditions;
+
+    @Before
+    public void setUp() throws IOException {
+
+        game = new Game(0);
+        game.addPlayer("peppe");
+        game.start(new DefaulEventManagerFake());
+        player = game.getGameStatus().getPlayers().get("peppe");
+
+        effectOnConditions = new EffectOnConditions(Resource.MILITARYPOINT,5,Resource.MONEY,null);
+
+    }
+
+
+    @Test
+    public void firstTest(){
+
+
+        effectOnConditions.execute(player,game);
+    }
+
+}
