439a767a8fedb5e59ae6c67ec62dcd3470eed5c7
GabrielePrato <gabrieleprato.work@gmail.com>
2017-07-03 11:35:16 +0200

    More Tests

---
+++ src/test/java/it/polimi/ingsw/GC_06/model/playerTools/FamilyMemberTest.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.GC_06.model.playerTools;
+
+import it.polimi.ingsw.GC_06.model.Dice.Dice;
+import it.polimi.ingsw.GC_06.model.Dice.DiceSet;
+import it.polimi.ingsw.GC_06.model.State.Game;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by gabri on 03/07/2017.
+ */
+public class FamilyMemberTest {
+    private FamilyMember[] familyMembers;
+    private Game game;
+    private Player player;
+    private String colour;
+    private int diceValue;
+
+    @Before
+    public void setUp() throws IOException {
+        game = new Game(0);
+        game.addPlayer("gabri");
+        player = game.getGameStatus().getPlayers().get("gabri");
+        familyMembers = player.getFamilyMembers();
+        colour = familyMembers[0].getDiceColor();
+    }
+
+    @Test
+    public void firstTest() throws IOException {
+        //System.out.print(colour);
+        DiceSet diceSet = game.getDiceSet();
+        Dice[] dices = diceSet.getDices();
+        colour = dices[0].getColor();
+        //System.out.print(colour);
+        assertTrue(familyMembers[0].getValue()==dices[0].getPoints());
+    }
+}
