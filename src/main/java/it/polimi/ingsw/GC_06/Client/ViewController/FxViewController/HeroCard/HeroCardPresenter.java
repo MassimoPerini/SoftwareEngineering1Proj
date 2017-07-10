f5a5e4789c14a0b02bb5d8c2250ba6614dbe4544
Massimo Perini <perinimassimo1995@gmail.com>
2017-07-10 11:11:38 +0200

    Fix GUI

--- src/main/java/it/polimi/ingsw/GC_06/Client/ViewController/FxViewController/HeroCard/HeroCardPresenter.java
+++ src/main/java/it/polimi/ingsw/GC_06/Client/ViewController/FxViewController/HeroCard/HeroCardPresenter.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.GC_06.Client.ViewController.FxViewController.HeroCard;
 
-import it.polimi.ingsw.GC_06.Client.Model.ClientFamilyMember;
 import it.polimi.ingsw.GC_06.Client.Model.ClientPlayerBoard;
 import it.polimi.ingsw.GC_06.Client.Network.ClientNetworkOrchestrator;
 import it.polimi.ingsw.GC_06.Server.Message.Client.PopUp.PlayerHeroCardChoices;
@@ -8,8 +7,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
@@ -19,7 +16,6 @@ import javafx.scene.image.ImageView;
 import javax.inject.Inject;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Observable;
 
 /**
  * Created by giuseppe on 7/9/17.
@@ -37,15 +33,13 @@ public class HeroCardPresenter {
     @Inject
     private ClientNetworkOrchestrator clientNetworkOrchestrator;
 
-    @FXML
-    public void init() {
+    @FXML void initialize() {
 
         ObservableList<String> listItems = FXCollections.observableList(clientPlayerBoard.getHeroCards());
 
         heroList.setItems(listItems);
         heroList.setCellFactory(param -> new ListCell<String>() {
             private ImageView imageView = new ImageView();
-
             @Override
             public void updateItem(String hero, boolean empty) {
                 super.updateItem(hero, empty);
@@ -53,6 +47,8 @@ public class HeroCardPresenter {
                     setText(null);
                     setGraphic(null);
                 } else {
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitHeight(150);
                     imageView.setImage(new Image(getClass().getResourceAsStream("/view/heroCard/" + hero  + ".jpg")));
                     setGraphic(imageView);
                 }
---
+++ src/main/java/it/polimi/ingsw/GC_06/Client/ViewController/FxViewController/PopUp/Disconnected/DisconnectedView.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.GC_06.Client.ViewController.FxViewController.PopUp.Disconnected;
+
+/**
+ * Created by massimo on 10/07/17.
+ */
+public class DisconnectedView {
+}
